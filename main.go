b1ed3057a0e986c8ef7f9cf60a3e771c5af9a0c6
anubanqwer <anuban12238@hotmail.com>
2021-01-14 14:29:42 +0700

    Fizzbuzz api

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-func main(){
-	println("Hello go")
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/anubanqwer/Go/fizzbuzz"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/fizzbuzz/:number", fizzbuzzHandler)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func fizzbuzzHandler(c echo.Context) error {
+	number := c.Param("number")
+
+	n, err := strconv.Atoi(number)
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	return c.String(http.StatusOK, fizzbuzz.Say(n))
 }
 
 /*
